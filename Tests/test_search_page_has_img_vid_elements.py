b85440de106f9cbd6aa6d74610439e4f6f178278
dante7928502 <dante7928502@mail.ru>
2026-01-12 17:39:48 +0300

    final version

--- Tests/test_search_page_has_img_vid_elements.py
+++ Tests/test_search_page_has_img_vid_elements.py
@@ -1,9 +1,9 @@
 from YandexPages import SearchHelper
 
-
 def test_yandex_search(browser):
     yandex_main_page = SearchHelper(browser)
     yandex_main_page.go_to_site()
+    yandex_main_page.close_popup_overlay()
     yandex_main_page.enter_word("poe")
     yandex_main_page.click_on_the_search_button()
     elements = yandex_main_page.check_navigation_bar()
--- YandexPages.py
+++ YandexPages.py
@@ -1,23 +1,40 @@
 from BaseApp import BasePage
 from selenium.webdriver.common.by import By
 
+
 class YandexSeacrhLocators:
     LOCATOR_YANDEX_SEARCH_FIELD = (By.ID, "text")
     LOCATOR_YANDEX_SEARCH_BUTTON = (By.CLASS_NAME, "search3__button")
-    LOCATOR_YANDEX_NAVIGATION_BAR = (By.CSS_SELECTOR, "a.HeaderNav-Tab")
+    LOCATOR_YANDEX_POPUP_OVERLAY_CLOSE_BTN = (
+        By.CLASS_NAME,
+        "DistributionButtonClose",
+    )
+    LOCATOR_YANDEX_NAVIGATION_BAR = (By.CLASS_NAME, "HeaderNav-Tab")
+
 
 class SearchHelper(BasePage):
 
+    def close_popup_overlay(self):
+        return self.find_element(
+            YandexSeacrhLocators.LOCATOR_YANDEX_POPUP_OVERLAY_CLOSE_BTN
+        ).click()
+
     def enter_word(self, word):
-        search_field = self.find_element(YandexSeacrhLocators.LOCATOR_YANDEX_SEARCH_FIELD)
+        search_field = self.find_element(
+            YandexSeacrhLocators.LOCATOR_YANDEX_SEARCH_FIELD
+        )
         search_field.click()
         search_field.send_keys(word)
         return search_field
 
     def click_on_the_search_button(self):
-        return self.find_element(YandexSeacrhLocators.LOCATOR_YANDEX_SEARCH_BUTTON,time=2).click()
+        return self.find_element(
+            YandexSeacrhLocators.LOCATOR_YANDEX_SEARCH_BUTTON, time=2
+        ).click()
 
     def check_navigation_bar(self):
-        all_list = self.find_elements(YandexSeacrhLocators.LOCATOR_YANDEX_NAVIGATION_BAR,time=2)
+        all_list = self.find_elements(
+            YandexSeacrhLocators.LOCATOR_YANDEX_NAVIGATION_BAR, time=2
+        )
         nav_bar_menu = [x.text for x in all_list if len(x.text) > 0]
         return nav_bar_menu
